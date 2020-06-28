c80656b8f2f5f26925ac8640fe23c9eb6284d818
Kevin Lin <kevin.lin.p@gmail.com>
2020-06-27 22:14:35 -0400

    adding select component

--- imports/ui/SongList.svelte
+++ imports/ui/SongList.svelte
@@ -4,6 +4,7 @@
   import { Songs as SongsUncached } from '../api/songs.js'
   import { onMount } from 'svelte'
   import { Ground } from 'meteor/ground:db'
+  import Select from 'svelte-select'
 
   import Song from './Song.svelte'
 
@@ -17,18 +18,36 @@
     Meteor.subscribe('songs')
   })
 
+  let selectedSong = null
+
+  const selectProps = {
+    isVirtualList: true,
+    // optionIdentifier: '_id',
+    // getOptionLabel: (option, filterText) => option.title,
+    // getSelectionLabel: (option) => option.title,
+  }
+
   $: songs = useTracker(() => Songs.find({}, {sort: {title: 1}}).fetch())
+  $: songSelectItems = $songs.map((s) => { return {label: s.title, value: s._id} })
+  $: console.log('ss', selectedSong)
 </script>
- 
+
+<div class="mt-5">
+  <Select items={songSelectItems} {...selectProps} bind:selectedSong></Select>
+</div>
+
  
 <header class="my-5">
-  <h1>Songs</h1>
+  <h1>
+  {#if selectedSong}
+    { selectedSong.title }
+  {/if}
+  </h1>
 </header>
 
 <ul class="list-unstyled">
 {#each $songs as song}
   <Song
-    key={song._id}
     song={song}
   />
 {/each}
